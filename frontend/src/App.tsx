a1fc30c384e30e169eb39cac686c5b661ec46ee7
Matheus Gabriel <matheusgmello1@gmail.com>
2022-11-14 22:53:14 -0300

    Other components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,18 @@
 import NotificationButton from "./components/NotificationButton";
+import Header from "./components/Header";
+import SalesCard from "./components/SalesCard";
 
 function App() {
   return (
     <>
-      <h1>Olá mundo</h1>
-      <NotificationButton />
+      <Header></Header>
+      <main>
+        <section id="sales">
+          <div className="dsmeta-container">
+            <SalesCard/>
+          </div>
+        </section>
+      </main>
     </>
   );
 }
