de71c774567f328a9d01c28dd95394d13c6f4770
Matheus Gabriel <matheusgmello1@gmail.com>
2022-11-18 09:51:40 -0300

    Sale listing

--- frontend/src/components/Header/index.tsx
+++ frontend/src/components/Header/index.tsx
@@ -9,7 +9,7 @@ function Header() {
         <h1>DSMeta</h1>
         <p>
           Desenvolvido por
-          <a href="https://www.instagram.com/devsuperior.ig">@devsuperior.ig</a>
+          <a href="https://www.instagram.com/devsuperior.ig" target={'_blank'}>@devsuperior.ig</a>
         </p>
       </div>
     </header>
